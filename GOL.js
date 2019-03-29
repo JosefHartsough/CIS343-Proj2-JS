0fedb7c9eda471da352a1f1f92ece52d52a28070
Josef Hartsough <36377458+JosefHartsough@users.noreply.github.com>
2019-03-29 14:18:23 -0400

    Update GOL.js

--- GOL.js
+++ GOL.js
@@ -1,24 +1,29 @@
-//*********************************************************
-//Filename: GOL.js
-//
-//Author: Josef Hartsough, Jarod Collier, Maz Ashgar
-//*********************************************************
+/*****************************************************************
+Conways Game Of Life 
 
-//File system for Node.js
+@authors Josef Hartsough, Jarod Collier,  Maz Ashgar
+@version Winter 2007
+*****************************************************************/
+
+/**File system for Node.js */
 var fs = require('fs');
-//Allows for input from terminal
+/**Allows for input from terminal*/
 var readlineSync = require('readline-sync');
 
-//Game Of Life class
+/** Game Of Life Class */
 class GameOfLife {
-    //Sets instance variables to default values
+    /** Sets instance variables to default values 
+    	rows and columns start at 0 */
     constructor() {
         this.grid = [];
         this.rows = 0;
         this.cols = 0;
     }
 
-	//Rows are x, Cols are y 
+    /*****************************************************************
+    Get grid generates the grid for Game Of Life
+    @param x are rows, y are colums
+    *****************************************************************/
     getGrid(x, y, data) { // get array of arrays
         this.rows = x;
         this.cols = y;
@@ -27,11 +32,13 @@ class GameOfLife {
         for (let i = 0; i < this.rows; i++) {
             this.grid[i] = new Array(y);
             for (let j = 0; j < this.cols; j++) {
-                this.grid[i][j] = data[offset++]; // copy data into grid
+                this.grid[i][j] = data[offset++]; /** Copys the data to the grid */
             }
         }
     }
-    //Prints the grid
+    /*****************************************************************
+    Prints the grid basef on # of rows and columns
+    *****************************************************************/
     printGrid() {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
@@ -40,8 +47,12 @@ class GameOfLife {
             console.log("\n"); 
         }
     }
-    //Saves the current grid to file
-    //Buffer used to handle binary data 
+    /*****************************************************************
+    Save gride saves the current gride to file
+    The buffer is useed to handle the binary data from the input file
+    (sample1.gol)
+    @param Sample1.gol
+    *****************************************************************/
     saveGrid(file) {
         var buffer = new Buffer(2 + this.rows*this.cols);
         buffer[0] = this.rows;
@@ -54,6 +65,10 @@ class GameOfLife {
 
         fs.writeFileSync(file, buffer);
     }
+    /*****************************************************************
+    Every cell interacts with its eight neighbours
+    that are horizontally, vertically, or diagonally adjacent
+    *****************************************************************/	
     mutate() {
        //Copy of original grid
         let temp_grid = new Array(this.rows);
@@ -64,23 +79,23 @@ class GameOfLife {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
                 let liveNeighbor = this.getNeighbors(i, j);
-                //a cell with less than 2 live neighbors dies
+                /** A cell with less than 2 live neighbors dies */
                 if (this.grid[i][j] !== 0 && liveNeighbor < 2) {
                     temp_grid[i][j] = 0;
                 }
-                //a cell with 2 or 3 live neighbors lives
+                /** a cell with 2 or 3 live neighbors lives */'
                 else if (this.grid[i][j] !== 0 && (liveNeighbor === 2 || liveNeighbor === 3)) {
                     temp_grid[i][j] = 1;
                 }
-                //a cell with greater than 3 live neighbors dies
+                /** a cell with greater than 3 live neighbors dies */
                 else if (this.grid[i][j] !== 0 && liveNeighbor > 3) {
                     temp_grid[i][j] = 0;
                 }
-                // a dead cell with 3 live neighbors becomes live(gets birthed)
+                /** a dead cell with 3 live neighbors becomes live(gets birthed) */
                 else if (this.grid[i][j] === 0 && liveNeighbor === 3) {
                     temp_grid[i][j] = 1;
                 }
-                // if we get this far then no changes required, so just copy this element
+                /** if we get this far then no changes required, so just copy this element */
                 else {
                     temp_grid[i][j] = this.grid[i][j];
                 }
@@ -89,19 +104,22 @@ class GameOfLife {
         this.grid = temp_grid;
     }
 
-    // Returns the number of live neighbors for cell at this.grid[i][j]
+    /*****************************************************************
+    getNeighbors returns the number of live neighbors a cell has.
+    *****************************************************************/	
+    Returns the number of live neighbors for cell at this.grid[i][j]
     getNeighbors(i, j) {
         let live_neighbors = 0;
         let x = this.rows;
         let y = this.cols;
-        // 3 neighbors on row above cell
+        /** 3 neighbors on row above cell */
         if (i-1 >= 0 && j-1 >= 0 && this.grid[i-1][j-1] > 0) live_neighbors++;   // test cell up-left
         if (i-1 >= 0             && this.grid[i-1][j]   > 0) live_neighbors++;   // test cell up
         if (i-1 >= 0 && j+1 < y  && this.grid[i-1][j+1] > 0) live_neighbors++;   // test cell up-right
-        // 2 neighbors on same row as cell
+        /** 2 neighbors on same row as cell */
         if (j-1 >= 0             && this.grid[i][j-1] > 0) live_neighbors++;     // test cell left
         if (j+1 <  y             && this.grid[i][j+1] > 0) live_neighbors++;     // test cell right
-        // 3 neighbors on row below cell
+        /** 3 neighbors on row below cell */
         if (i+1 < x && j-1 >= 0  && this.grid[i+1][j-1] > 0) live_neighbors++;    // test cell down-left
         if (i+1 < x              && this.grid[i+1][j] > 0)   live_neighbors++;    // test cell down
         if (i+1 < x && j+1 < y   && this.grid[i+1][j+1] > 0) live_neighbors++;    // test cell down-right
@@ -109,10 +127,13 @@ class GameOfLife {
     }
 }
 
+/*****************************************************************
+Main function for Game of Life
+*****************************************************************/
 main();
 
 function main() {
-    // program requires a .gol file name
+    /** program requires a .gol file name (sample1.gol or sample2.gol) */
     if (process.argv.length != 3) {
         console.log("This program requires 3 parameters: node GOL.js sample1.gol");
         process.exit(1)
@@ -121,10 +142,10 @@ function main() {
     console.log("script: " + process.argv[1]);
     console.log("data: " + process.argv[2]);
 
-	//Game of life object 
+	/** New Game of Life Object */ 
     let gol = new GameOfLife();
 
-    // This buffer will hold the bytes read from the file.    
+    /** This buffer will hold the bytes read from the file */ 
     fs.open(process.argv[2], 'r', function (err, fd) {
         let data = [];
         if (err)
@@ -137,20 +158,21 @@ function main() {
             data.push(buffer[0]);
           
         }
-        // The first and second byte in the file is height and width.
+        /**The first and second byte in the file is height and width
+	 x is the height, y is the width.                         */
         let x = data[0];
         let y = data[1];
 
-        // grid holds the game board.  The game board
-        // is x arrays of arrays of length y
-        // Rows are x, Cols are y
+        /**grid holds the game board.  The game board
+        is x arrays of arrays of length y
+        Rows are x, Cols are y 			   */
         console.log("\nGrid size %d, %d.\n", x, y);
         gol.getGrid(x, y, data);        
         gol.printGrid();
 
-        //Choices for user 
+        /** Choices for user */
         while (true) {
-            //Takes the input of the user
+            /** Takes the input of the user */
             let line = readlineSync.question(
                 "Press return for next generation,\n" +
                 "n to iterate multiple times,\n" +
@@ -163,13 +185,13 @@ function main() {
                     console.log("Exiting program");
                     process.exit(0);
                     break;
-                //Saves
+                /** Saves */
                 case "w":
                     let filename = readlineSync.question("Enter a filename: ");
                     gol.saveGrid(filename.trim());
                     console.log("Grid saved to file " + filename + "\n");
                     break;
-				//Iterates through n times of generations
+				/** Iterates through n times of generations */
                 case "n":
                     let num = parseInt(readlineSync.question("How many iterations? "));
                     console.log();
